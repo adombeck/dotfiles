0897437810e6ee1d778442277659382f000d5cd9
Adrian Dombeck <adrian.dombeck@rub.de>
2018-05-15 19:01:46 +0200

    Add install.sh which adds bind mount entries to fstab

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function ensure_mount_point_exists {	
+	src=$1
+	mountpoint=$2
+	[ -e "$mountpoint" ] && return
+	if [ -d "$src" ]; then
+		sudo mkdir "$mountpoint"
+	else
+		sudo touch "$mountpoint"
+	fi
+}
+
+function add_to_fstab {
+	src=$1
+	mountpoint=$2
+	grep -q "$src" /etc/fstab || echo "$src $mountpoint none defaults,bind 0 0" | sudo tee -a /etc/fstab > /dev/null
+}
+
+for f in $DIR/.[!.]*; do
+	if [[ "$f" == "$DIR/.git"* ]]; then
+		echo "Skipping $f"
+		continue
+	fi
+	mountpoint="$(realpath ~/$(basename $f))"
+	echo "Installing $f to $mountpoint"
+	ensure_mount_point_exists $f $mountpoint
+	add_to_fstab $f $mountpoint
+done
